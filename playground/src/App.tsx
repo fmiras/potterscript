bb813b405cbb58ded78a2bcb1e9e05cae95619cd
Federico Miras <miras.federico@gmail.com>
2023-08-28 22:42:13 -0300

    fix: clean playground app

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { parse_potterscript } from 'potterscript-wasm'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const defaultCode = `index = 0
@@ -21,35 +19,26 @@ quidditch {
 }`
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [code] = useState(defaultCode)
+  const [code, setCode] = useState(defaultCode)
+  const [ast, setAst] = useState(parse_potterscript(code))
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>PotterScript Playground</h1>
+      <div className="container">
+        <div className="editor">
+          <textarea
+            value={code}
+            onChange={(e) => {
+              setCode(e.target.value)
+              setAst(parse_potterscript(e.target.value))
+            }}
+          ></textarea>
+        </div>
+        <div className="ast">
+          <pre>{JSON.stringify(ast, null, 2)}</pre>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          <strong>Code:</strong>
-        </p>
-        <pre>{code}</pre>
-        <p>
-          <strong>Output:</strong>
-        </p>
-        <pre>{parse_potterscript(code)}</pre>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
