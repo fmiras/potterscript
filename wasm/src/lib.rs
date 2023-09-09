67302ac9d4828f42268a1a7ff6081a2453d1c771
Federico Miras <miras.federico@gmail.com>
2023-09-09 13:22:21 -0300

    test(parse_program): move from wasm package to parser

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -7,32 +7,9 @@ pub fn parse_potterscript(code: &str) -> String {
     serde_json::to_string(&ast).unwrap()
 }
 
-#[test]
-fn test_parse_potterscript() {
-    let code = r#"index = 0
-
-    quidditch {
-      snake = ~Serpensortia
-      ~WingardiumLeviosa snake
-      ~WingardiumLeviosa snake
-      snake = snake + " some string"
-      ~Revelio snake
-      ~Incendio snake
-      ~Revelio snake
-      ~Engorgio index
-    
-      if index == 4 {
-        snitch # Break loop
-      }
-    }
-    "#;
-    let ast = potterscript_parser::parse_program(code).unwrap();
-    let json = serde_json::to_string(&ast).unwrap();
-
-    println!("{}", json);
-
-    assert_eq!(
-        json,
-        r#"["",[{"VariableAssignment":["index",{"Atom":{"Integer":0}}]},{"Quidditch":[{"VariableAssignment":["snake",{"SpellCast":["Serpensortia",null]}]},{"ExpressionStatement":{"SpellCast":["WingardiumLeviosa",{"Atom":{"Variable":"snake"}}]}},{"ExpressionStatement":{"SpellCast":["WingardiumLeviosa",{"Atom":{"Variable":"snake"}}]}},{"VariableAssignment":["snake",{"BinaryOperation":["Plus",{"Atom":{"Variable":"snake"}},{"Atom":{"String":" some string"}}]}]},{"ExpressionStatement":{"SpellCast":["Revelio",{"Atom":{"Variable":"snake"}}]}},{"ExpressionStatement":{"SpellCast":["Incendio",{"Atom":{"Variable":"snake"}}]}},{"ExpressionStatement":{"SpellCast":["Revelio",{"Atom":{"Variable":"snake"}}]}},{"ExpressionStatement":{"SpellCast":["Engorgio",{"Atom":{"Variable":"index"}}]}},{"If":[{"BinaryOperation":["Equal",{"Atom":{"Variable":"index"}},{"Atom":{"Integer":4}}]},["Snitch",{"ExpressionStatement":{"Comment":" Break loop"}}],[]]}]}]]"#
-    );
-}
+// #[wasm_bindgen]
+// pub fn parse_and_run(code: &str) {
+//     let (_, program) = potterscript_parser::parse_program(code).unwrap();
+//     let mut runtime = potterscript_runtime::Runtime::new();
+//     runtime.eval(program);
+// }
